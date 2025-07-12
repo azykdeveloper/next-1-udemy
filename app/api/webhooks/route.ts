c387cdea9e05959880bc1e1433faef58d538c0cd
azykdeveloper <azykdeveloper@gmail.com>
2025-07-12 22:29:30 +0500

    c

--- app/api/webhooks/route.ts
+++ app/api/webhooks/route.ts
@@ -1,46 +1,48 @@
 import { createUser, updateUser } from "@/actions/user.action";
 import { WebhookEvent } from "@clerk/nextjs/server";
+import { verifyWebhook } from "@clerk/nextjs/webhooks";
 import { headers } from "next/headers";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { Webhook } from "svix";
 
-export async function POST(req: Request) {
-  const WEBHOOK_SECRET = process.env.NEXT_CLERK_WEBHOOK_SECRET;
-  console.log(WEBHOOK_SECRET);
-  if (!WEBHOOK_SECRET) {
-    throw new Error("WEBHOOK_SECRET is not defined");
-  }
+export async function POST(req: NextRequest) {
+  // const WEBHOOK_SECRET = process.env.CLERK_WEBHOOK_SIGNING_SECRET;
+  
+  // if (!WEBHOOK_SECRET) {
+  //   throw new Error("WEBHOOK_SECRET is not defined");
+  // }
 
-  const headerPayload = await headers();
-  const svixId = headerPayload.get("svix-id");
-  const svixTimestamp = headerPayload.get("svix-timestamp");
-  const svixSignature = headerPayload.get("svix-signature");
+  // const headerPayload = await headers();
+  // const svixId = headerPayload.get("svix-id");
+  // const svixTimestamp = headerPayload.get("svix-timestamp");
+  // const svixSignature = headerPayload.get("svix-signature");
 
-  if (!svixId || !svixTimestamp || !svixSignature) {
-    return new Response("Error occured -- no svix headers", {
-      status: 400,
-    });
-  }
+  // if (!svixId || !svixTimestamp || !svixSignature) {
+  //   return new Response("Error occured -- no svix headers", {
+  //     status: 400,
+  //   });
+  // }
 
-  const payload = await req.json();
-  const body = JSON.stringify(payload);
+  // const payload = await req.json();
+  // const body = JSON.stringify(payload);
 
-  const wh = new Webhook(WEBHOOK_SECRET);
+  // const wh = new Webhook(WEBHOOK_SECRET);
 
-  let evt: WebhookEvent;
+  const evt = await verifyWebhook(req);
 
-  try {
-    evt = wh.verify(body, {
-      "svix-id": svixId,
-      "svix-timestamp": svixTimestamp,
-      "svix-signature": svixSignature,
-    }) as WebhookEvent;
-  } catch (err) {
-    console.error("Error verifying webhook:", err);
-    return new Response("Error occured", {
-      status: 400,
-    });
-  }
+
+  // try {
+  //   evt = wh.verify(body, {
+  //     "svix-id": svixId,
+  //     "svix-timestamp": svixTimestamp,
+  //     "svix-signature": svixSignature,
+  //   }) as WebhookEvent;
+  // } catch (err) {
+  //   console.error("Error verifying webhook:", err);
+  //   return new Response("Error occured", {
+  //     status: 400,
+  //   });
+  // }
 
   const eventType = evt.type;
 
