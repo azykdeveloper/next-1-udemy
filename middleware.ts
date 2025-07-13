132f6cf41ebc36765acf093afe5823836b812725
azykdeveloper <azykdeveloper@gmail.com>
2025-07-13 11:17:23 +0500

    oo1

--- middleware.ts
+++ middleware.ts
@@ -9,7 +9,7 @@ const isProtectedRoute = createRouteMatcher(["/:locale/dashboard(.*)"]);
 
 // 👇 Auth dan ozod qilinadigan yo‘llar
 const isIgnoredRoute = createRouteMatcher([
-  "/api/webhooks", // 👈 bu yerda webhook route ni ignore qilyapmiz
+  "/api/webhook", // 👈 bu yerda webhook route ni ignore qilyapmiz
 ]);
 
 export default clerkMiddleware(async (auth, req) => {
